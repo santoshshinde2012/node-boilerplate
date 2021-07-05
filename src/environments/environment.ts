df544a262666557ab319b3b08339647436e0ca11
Santosh Shinde <santoshshinde2012@gmail.com>
2021-07-05 16:31:41 +0530

    clear repo

--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-import environmentConfiguration from './environment.local';
-
-export default function getConfiguration() {
-  const environment = process.env.NODE_ENV;
-  if (environment) {
-    return environmentConfiguration;
-  }
-  throw new Error('Env not set');
-}
