9023566544e1ddadd8a26072ee97d3fc70642d51
Santosh Shinde <santoshshinde2012@gmail.com>
2022-10-20 16:37:04 +0530

    fixed test-report.xml missing

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,12 @@ module.exports = {
   roots: [
     "./tests"
   ],
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/tests/"
+  ],
+  collectCoverage: true,
   testEnvironment: 'node',
+  testResultsProcessor: "jest-sonar-reporter",
   setupFiles: ['dotenv/config'],
 };
