36d2c352ca4396fe90b657f5ce7ce773811353a0
Santosh Shinde <santoshshinde2012@gmail.com>
2024-01-16 00:22:19 +0530

    update mail lib

---
+++ src/lib/mailer.ts
@@ -0,0 +1,35 @@
+import nodemailer from 'nodemailer';
+
+export default class Mailer {
+    static async sendMail({ to, subject, body }) {
+        try {
+            let transporter = nodemailer.createTransport({
+                host: process.env.MAIL_HOST,
+                port: Number(process.env.MAIL_PORT) || 0,
+                secure: false, // true for 465, false for other ports
+                auth: {
+                    user: process.env.MAIL_EMAIL, // generated ethereal user
+                    pass: process.env.MAIL_PASSWORD, // generated ethereal password
+                },
+                tls: {
+                    minVersion: 'TLSv1',
+                    rejectUnauthorized: false,
+                },
+            });
+
+            // send mail with defined transport object
+            let info = await transporter.sendMail({
+                from: process.env.MAIL_EMAIL, // sender address
+                to: to, // list of receivers
+                subject: subject, // Subject line
+                text: body, // plain text body
+                html: body, // html body
+            });
+
+            console.log('Message sent: %s', info.messageId);
+        } catch (error) {
+            console.log('Problem when trying send email', error);
+        }
+
+    }
+}
